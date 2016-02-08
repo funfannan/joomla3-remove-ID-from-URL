6c4b8930d9289cde826fc1004d757655967409a5
funfannan <web@stileliberoagenzia.com>
2016-02-08 11:36:56 +0100

    Update router.php

--- router.php
+++ router.php
@@ -1,5 +1,5 @@
 // In /components/com_content/router.php replace this:
-//$advanced = $params->get('sef_advanced_link', 0);
-//With this two times:
+// $advanced = $params->get('sef_advanced_link', 0);
+// With this two times:
 
 $advanced = $params->get('sef_advanced_link', 1);
