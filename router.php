936fe766ff62edb77f25ec1e4d7c36ab2d056717
funfannan <web@stileliberoagenzia.com>
2016-02-11 10:57:28 +0100

    Update router.php

--- router.php
+++ router.php
@@ -3,3 +3,21 @@
 // With this two times:
 
 $advanced = $params->get('sef_advanced_link', 1);
+
+// AND comment out
+if (strpos($segments[0], ':') === false)
+{
+	$vars['view'] = 'article';
+	$vars['id'] = (int) $segments[0];
+
+	return $vars;
+}
+
+// LIKE
+//			/*if (strpos($segments[0], ':') === false)
+//			{
+//				$vars['view'] = 'article';
+//				$vars['id'] = (int) $segments[0];
+//
+//				return $vars;
+//			}*/
